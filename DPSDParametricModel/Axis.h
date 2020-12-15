9665bf63a4848aabe26a9fbb579e7b57dc59b05f
Alexander Agathos <alexander@agathoscompany.com>
2020-12-15 21:20:19 +0200

    Axis doxygen comments

--- DPSDParametricModel/Axis.h
+++ DPSDParametricModel/Axis.h
@@ -13,6 +13,9 @@
 class Axis
 {
 protected:
+	
+	// Axis feature three used to define the axis.
+	// In this case the axis will be defined by the intersection of two planes
 	ElemTreeData axis_tree[9] = {
 		/* 0 */ {0, PRO_E_FEATURE_TREE, {(ProValueDataType)-1}},
 		/* 1 */ {1, PRO_E_FEATURE_TYPE, {PRO_VALUE_TYPE_INT, PRO_FEAT_DATUM_AXIS}},
@@ -24,11 +27,43 @@ protected:
 		/* 7 */ {3, PRO_E_DTMAXIS_CONSTR_TYPE, {PRO_VALUE_TYPE_INT, 1}},
 		/* 8 */ {3, PRO_E_DTMAXIS_CONSTR_REF, {PRO_VALUE_TYPE_SELECTION}}
 	};
-	int axis_id;
+	
+	int axis_id; /**< This is the axis id, throughout the example it should not change even
+						when it is redefined by the dialog */
+
+	/**
+	 * Set the axis intersecting planes
+	 * 
+	 * @param model The input part session of the creo parametric environment
+	 * @param planeIdFirst The first intersecting plane id
+	 * @param planeIdSecond The second intersecting plane id
+	 */
 	void setInstsectionPlanes(ProMdl model, int planeIdFirst, int planeIdSecond);
+
+	/**
+	 * Create the axis after the two intersecting planes has been provided
+	 * 
+	 * @param model The input part session of the creo parametric environment
+	 * @param name The name that the axis will have in creo.
+	 * @return a status value
+	 */
 	int createAxis(ProMdl model, const char* name);
+
 public:
+	/**
+	 * Constructor of the axis class
+	 * 
+	 * @param model The input part session of the creo parametric environment
+	 * @param planeIdFirst The first intersecting plane id
+	 * @param planeIdSecond The second intersecting plane id
+	 */
 	Axis(ProMdl model, int planeIdFirst, int planeIdSecond, const char* name);
+
+	/**
+	 * Get axis id.
+	 *
+	 * @return The axis id
+	 */
 	int getAxisid();
 	
 };
